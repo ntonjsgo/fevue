8714d9cdd25270882de5411d51a48425c703aecd
Federico Greco <fgreco@actiaitalia.com>
2022-03-01 11:15:37 +0100

    CreateUser

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -11,14 +11,22 @@
                     <input type="text" class="form-control" id="name-input" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"  required>
                 </div>
             </div>
+            <div v-if="!isLogin" class="col-12 d-flex-justify-content-center">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" id="email-input" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"  required>
+                </div>
+            </div>
             <div class="col-12 d-flex-justify-content-center">
                 <div class="input-group mb-3">
-                    <input type="password" class="form-control"  placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" >
+                    <input type="password" class="form-control"  placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" required>
                 </div>
             </div>
             <div class="offset-3 col-6 d-flex-justify-content-center">
-                <button class="w-100 btn btn-lg bg-gradient text-white" type="submit" id="login" @click="fetchUser">Login</button>
+                <button v-if="isLogin" class="w-100 btn btn-lg bg-gradient text-white" type="submit" id="login" @click="fetchUser">Login</button>
+                <button v-else class="w-100 btn btn-lg bg-gradient text-white" type="submit" id="signup" @click="createUser">Signup</button>
             </div>
+            <a class="login-toggler mt-3" v-if="isLogin" @click="isLogin = !isLogin">Non hai un account? Registrati!</a>
+            <a v-else class="login-toggler mt-3" @click="isLogin = !isLogin">Hai già un account? Fai un login!</a>
         </div>
 
     </div>
@@ -32,7 +40,8 @@ export default {
     name: 'Auth',
     data(){
         return{
-            url : "http://localhost:8080"
+            url : "http://localhost:8080",
+            isLogin: true
         }
     },
     methods: {
@@ -54,6 +63,20 @@ export default {
 
 
 
+        }, 
+        createUser: function() {
+            let name = document.getElementById('name-input').value;
+            let email = document.getElementById('email-input').value;
+            let payload = {
+                "name": name,
+                "email": email
+            };
+            axios.post(this.url + "/api/customers/create", payload).then((res)=> {
+                localStorage.setItem('username', res.data.name);
+                     localStorage.setItem('userid', res.data.id);
+                     localStorage.setItem('useremail', res.data.email);
+                     window.location.href = '/home';
+            })
         }
     },
     mounted: function() {}
@@ -67,5 +90,20 @@ body{
     justify-content: center;
     align-items: center;
 
+}
+
+a {
+    color: black;
+    text-decoration: none;
+}
+
+a:hover {
+    color:#b82c2c; 
+    text-decoration:none; 
+    cursor:pointer;  
+}
+
+.login-toggler {
+    text-align: center;
 }
 </style>
